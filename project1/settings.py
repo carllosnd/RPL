5155d2c77484e6db26547fe6d1aaabfd23259fc7
carlos.daeli <carlosdaeli@gmail.com>
2023-02-23 22:30:56 +0700

    2nd commit

--- project1/settings.py
+++ project1/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'project1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'rpl',
+        'USER': 'root',
+        'PASSWORD': 'carlos2004',
+        'HOST': 'localhost',
+        'PORT': '3307'
     }
 }
 
